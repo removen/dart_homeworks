616e77304f7bc7c8b0bce129b18f49931233c0bf
remove <rayongold573@gmail.com>
2025-11-10 19:09:01 +0500

    1.1

---
+++ Lesson5/task.dart
@@ -0,0 +1,8 @@
+void main () {
+  String city = "Кызылорда" ;
+  int age = 17;
+  String name = "Асылжан" ;
+  bool isStydent = true;
+
+  print("Менін атым $name. Маған $age жас, менін қалам $city. Мен студентпім $isStydent");
+}
