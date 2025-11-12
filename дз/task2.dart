c06e93ba28655d35aea3aa7013fcb5a76d9cc0a0
reamove <eralin00@bk.ru>
2025-11-12 23:51:21 +0500

    Add files via upload

---
+++ дз/task2.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 12;
+  int b = 5;
+  print('a + b = ${a + b}');
+  print('a - b = ${a - b}');
+  print('a * b = ${a * b}');
+  print('a / b = ${a / b}');
+  print('a ~/ b = ${a ~/ b}');
+  print('a % b = ${a % b}');
+}
